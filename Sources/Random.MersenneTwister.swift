982b3674f981ce9ff623dead469a1cc22edb97c7
Xiaodi Wu <xiaodi.wu@gmail.com>
2017-05-18 02:10:57 -0500

    Change visibility of MT19937 parameters

--- Sources/Random.MersenneTwister.swift
+++ Sources/Random.MersenneTwister.swift
@@ -11,18 +11,22 @@ extension Random {
   ///
   /// [ref]: https://doi.org/10.1145%2F272991.272995
   /* public */ final class MersenneTwister : PRNG {
-    // TODO: Document these static properties.
-    public static let (w, n, m) = (32, 624, 397)
-    public static let (r, a) = (31, 0x9908b0df as UInt32)
-    public static let (u, d) = (11, 0xffffffff as UInt32)
-    public static let (s, b) = ( 7, 0x9d2c5680 as UInt32)
-    public static let (t, c) = (15, 0xefc60000 as UInt32)
-    public static let l = 18
-    public static let f = 1812433253 as UInt32
-    internal static let _lower = (1 as UInt32 &<< r) &- 1
+    internal static let (_w, _n, _m) = (32, 624, 397)
+    internal static let (_r, _a) = (31, 0x9908b0df as UInt32)
+    // -------------------------------------------------------------------------
+    // Masks.
+    internal static let _lower = (1 as UInt32 &<< _r) &- 1
     internal static let _upper = ~_lower
+    // -------------------------------------------------------------------------
+    internal static let (_u, _d) = (11, 0xffffffff as UInt32)
+    internal static let (_s, _b) = ( 7, 0x9d2c5680 as UInt32)
+    internal static let (_t, _c) = (15, 0xefc60000 as UInt32)
+    internal static let _l = 18
+    // -------------------------------------------------------------------------
+    // Initialization multiplier.
+    internal static let _f = 1812433253 as UInt32
+    // -------------------------------------------------------------------------
 
-    // TODO: Document this property.
     internal var _state: (array: [UInt32], index: Int)
 
     public var state: (array: [UInt32], index: Int) {
@@ -31,32 +35,31 @@ extension Random {
       }
       set {
         precondition(
-          newValue.array.count == MersenneTwister.n &&
-            (0...MersenneTwister.n).contains(newValue.index)
+          newValue.array.count == MersenneTwister._n &&
+            (0...MersenneTwister._n).contains(newValue.index)
         )
         _state = newValue
       }
     }
 
-    // TODO: Document this function.
     internal func _twist() {
-      for i in 0..<MersenneTwister.n {
+      for i in 0..<MersenneTwister._n {
         let x = (_state.array[i] & MersenneTwister._upper) &+
-          (_state.array[(i &+ 1) % MersenneTwister.n] & MersenneTwister._lower)
-        let xA = x % 2 == 0 ? x &>> 1 : (x &>> 1) ^ MersenneTwister.a
+          (_state.array[(i &+ 1) % MersenneTwister._n] & MersenneTwister._lower)
+        let xA = x % 2 == 0 ? x &>> 1 : (x &>> 1) ^ MersenneTwister._a
         _state.array[i] =
-          _state.array[(i &+ MersenneTwister.m) % MersenneTwister.n] ^ xA
+          _state.array[(i &+ MersenneTwister._m) % MersenneTwister._n] ^ xA
       }
       _state.index = 0
     }
 
     public func next() -> UInt32? {
-      if _state.index == MersenneTwister.n { _twist() }
+      if _state.index == MersenneTwister._n { _twist() }
       var y = _state.array[_state.index]
-      y ^= (y &>> MersenneTwister.u) & MersenneTwister.d
-      y ^= (y &<< MersenneTwister.s) & MersenneTwister.b
-      y ^= (y &<< MersenneTwister.t) & MersenneTwister.c
-      y ^= (y &>> MersenneTwister.l)
+      y ^= (y &>> MersenneTwister._u) & MersenneTwister._d
+      y ^= (y &<< MersenneTwister._s) & MersenneTwister._b
+      y ^= (y &<< MersenneTwister._t) & MersenneTwister._c
+      y ^= (y &>> MersenneTwister._l)
       _state.index += 1
       return y
     }
@@ -67,21 +70,21 @@ extension Random {
     /// - Parameters:
     ///   - seed: The value to be used as the seed.
     public init(seed: UInt32) {
-      var array = [UInt32](repeating: 0, count: MersenneTwister.n)
+      var array = [UInt32](repeating: 0, count: MersenneTwister._n)
       array[0] = seed
-      for i in 1..<MersenneTwister.n {
+      for i in 1..<MersenneTwister._n {
         let previous = array[i &- 1]
         array[i] = UInt32(i) &+
-          MersenneTwister.f &*
-            (previous ^ (previous &>> (MersenneTwister.w &- 2)))
+          MersenneTwister._f &*
+            (previous ^ (previous &>> (MersenneTwister._w &- 2)))
       }
-      self._state = (array: array, index: MersenneTwister.n)
+      self._state = (array: array, index: MersenneTwister._n)
     }
 
     public init(state: (array: [UInt32], index: Int)) {
       precondition(
-        state.array.count == MersenneTwister.n &&
-          (0...MersenneTwister.n).contains(state.index)
+        state.array.count == MersenneTwister._n &&
+          (0...MersenneTwister._n).contains(state.index)
       )
       self._state = state
     }
