904eb7adca0391b3d8233fc3f6e4c3a77c4843f7
Xiaodi Wu <xiaodi.wu@gmail.com>
2017-06-02 18:37:28 -0500

    Revert "Update to avoid heterogeneous comparison"
    This reverts commit 042ae884abecfabc9f655041da8eea7d58854bfd.

--- Sources/Factoring.swift
+++ Sources/Factoring.swift
@@ -10,23 +10,23 @@ extension UnsignedInteger {
   // @_transparent // @_inlineable
   public static func gcd(_ a: Self, _ b: Self) -> Self {
     // An iterative version of Stein's algorithm.
-    if a == (0 as Self) { return b } // gcd(0, b) == b
-    if b == (0 as Self) { return a } // gcd(a, 0) == a
+    if a == 0 { return b } // gcd(0, b) == b
+    if b == 0 { return a } // gcd(a, 0) == a
 
     var a = a, b = b, shift = 0 as Self
-    while ((a | b) & 1) == (0 as Self) {
+    while ((a | b) & 1) == 0 {
       a &>>= 1
       b &>>= 1
       shift += 1
     }
     // Now, shift is equal to log2(k), where k is the greatest power of 2
     // dividing a and b.
-    while (a & 1) == (0 as Self) { a &>>= 1 } // Now, a is odd.
+    while (a & 1) == 0 { a &>>= 1 } // Now, a is odd.
     repeat {
-      while (b & 1) == (0 as Self) { b &>>= 1 } // Now, b is odd.
+      while (b & 1) == 0 { b &>>= 1 } // Now, b is odd.
       if a > b { swap(&a, &b) } // Now, a < b.
       b -= a
-    } while b != (0 as Self)
+    } while b != 0
     // Restore common factors of 2.
     return a &<< shift
   }
@@ -34,7 +34,7 @@ extension UnsignedInteger {
   /// Returns the least common multiple of `a` and `b`.
   @_transparent // @_inlineable
   public static func lcm(_ a: Self, _ b: Self) -> Self {
-    if a == (0 as Self) || b == (0 as Self) { return 0 }
+    if a == 0 || b == 0 { return 0 }
     return a / .gcd(a, b) * b
   }
 }
@@ -45,9 +45,7 @@ extension UnsignedInteger where Self : FixedWidthInteger {
   // @_transparent // @_inlineable
   public static func lcmReportingOverflow(_ a: Self, _ b: Self)
     -> (partialValue: Self, overflow: ArithmeticOverflow) {
-    if a == (0 as Self) || b == (0 as Self) {
-      return (partialValue: 0, overflow: .none)
-    }
+    if a == 0 || b == 0 { return (partialValue: 0, overflow: .none) }
     return (a / .gcd(a, b)).multipliedReportingOverflow(by: b)
   }
 
@@ -56,7 +54,7 @@ extension UnsignedInteger where Self : FixedWidthInteger {
   // @_transparent // @_inlineable
   public static func lcmFullWidth(_ a: Self, _ b: Self)
     -> (high: Self, low: Self.Magnitude) {
-    if a == (0 as Self) || b == (0 as Self) { return (0, 0) }
+    if a == 0 || b == 0 { return (0, 0) }
     return (a / .gcd(a, b)).multipliedFullWidth(by: b)
   }
 }
@@ -86,7 +84,7 @@ where Self : FixedWidthInteger, Magnitude : FixedWidthInteger & UnsignedInteger,
     let t = Self(extendingOrTruncating: Magnitude.gcd(a.magnitude, b.magnitude))
     return (
       partialValue: t,
-      overflow: ArithmeticOverflow(t < (0 as Self))
+      overflow: ArithmeticOverflow(t < 0)
     )
   }
 
@@ -99,7 +97,7 @@ where Self : FixedWidthInteger, Magnitude : FixedWidthInteger & UnsignedInteger,
     let u = Self(extendingOrTruncating: t)
     return (
       partialValue: u,
-      overflow: ArithmeticOverflow(overflow == .overflow || u < (0 as Self))
+      overflow: ArithmeticOverflow(overflow == .overflow || u < 0)
     )
   }
 
