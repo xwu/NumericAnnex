d2d23466423f0cd2e1f622326d5296d580409a57
Xiaodi Wu <xiaodi.wu@gmail.com>
2017-06-05 22:09:55 -0500

    Update to newest PackageDescription

--- Package.swift
+++ Package.swift
@@ -1,7 +1,25 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
-  name: "NumericAnnex"
+  name: "NumericAnnex",
+  products: [
+    .library(
+      name: "NumericAnnex",
+      targets: ["NumericAnnex"]
+    ),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "NumericAnnex",
+      dependencies: [],
+      path: "Sources"
+    ),
+    .testTarget(
+      name: "NumericAnnexTests",
+      dependencies: ["NumericAnnex"]
+    ),
+  ]
 )
